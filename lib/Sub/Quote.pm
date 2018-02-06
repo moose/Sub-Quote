e720ae0dbbc87236d37d9c0780f58989e5e5ab93
Graham Knop <haarg@haarg.org>
2018-02-06 19:05:54 +0100

    Bumping version to 2.005000

--- lib/Sub/Quote.pm
+++ lib/Sub/Quote.pm
@@ -16,7 +16,7 @@ BEGIN {
   *_HAVE_PERLSTRING = defined &B::perlstring ? sub(){1} : sub(){0};
 }
 
-our $VERSION = '2.004000';
+our $VERSION = '2.005000';
 $VERSION = eval $VERSION;
 
 our @EXPORT = qw(quote_sub unquote_sub quoted_from_sub qsub);

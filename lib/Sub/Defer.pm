896f05a5c5c09a4b7c3e1c305d31509c5ad76675
Graham Knop <haarg@haarg.org>
2019-01-07 15:43:04 +0100

    Bumping version to 2.006_001

--- lib/Sub/Defer.pm
+++ lib/Sub/Defer.pm
@@ -5,7 +5,7 @@ use Exporter qw(import);
 use Scalar::Util qw(weaken);
 use Carp qw(croak);
 
-our $VERSION = '2.006_000';
+our $VERSION = '2.006_001';
 $VERSION = eval $VERSION;
 
 our @EXPORT = qw(defer_sub undefer_sub undefer_all);
